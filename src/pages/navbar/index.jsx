cc9a7be6ab5b19761e02f95dd2d6d30e9f92e7d1
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2023-01-29 01:21:22 -0500

    Created new component called FlexBetween

--- src/pages/navbar/index.jsx
+++ src/pages/navbar/index.jsx
@@ -1,3 +1,25 @@
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close
+} from "@mui/icons-material";
+
 const Navbar = () => {
   return <div>nabar</div>;
 };
