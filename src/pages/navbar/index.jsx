7032c604e1bd10f6b6b2b99a7d4f11d434920b23
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2023-01-30 11:49:07 -0500

    Imported all the files we need for the Navbar

--- src/pages/navbar/index.jsx
+++ src/pages/navbar/index.jsx
@@ -19,8 +19,28 @@ import {
   Menu,
   Close
 } from "@mui/icons-material";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // This user caomes from the global state (Redux initial state)
+  const user = useSelector((state) => state.user);
+  // UseMediaQuery is similar to the CSS media querty for the breakpoint
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px");
+
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  const fullName = `${user.firstName} ${user.lastName}`;
+
   return <div>nabar</div>;
 };
 
